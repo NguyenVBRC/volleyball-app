546eb50642812d21f52b1eff94237e2317517885
Jimmy Nguyen <nguyen89.mke@gmail.com>
2025-02-14 14:23:06 -0600

    Waterfall

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Banner</h1>
+      <h1 className={styles.bannerContainer}>Banner</h1>
+      <div className={styles.homepage_links}>
+        <Link id={styles.linkCards} href="/tournaments">All Tournaments</Link>
+        <Link id={styles.linkCards} href="/leagues">All Leagues</Link>
+      </div>
     </div>
   );
 }
---
+++ src/app/rules/page.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+const RulesPage = () => {
+
+    return (
+        <h1>Rules Page</h1>
+    )
+}
+
+export default RulesPage;
